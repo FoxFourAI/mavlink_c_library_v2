74c9705e03073a88cf75957fbd58908996f3a58c
Derker2002 <siklerzp@gmail.com>
2026-05-26 14:54:13 +0200

    redefine message

--- AVSSUAS/AVSSUAS.h
+++ AVSSUAS/AVSSUAS.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_AVSSUAS.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_AVSSUAS_XML_HASH -8542179321304892832
+#define MAVLINK_AVSSUAS_XML_HASH -2717991522165531323
 
 #ifdef __cplusplus
 extern "C" {
--- marsh/marsh.h
+++ marsh/marsh.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_MARSH.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_MARSH_XML_HASH 8680255302906527444
+#define MAVLINK_MARSH_XML_HASH -7009294860614563353
 
 #ifdef __cplusplus
 extern "C" {
