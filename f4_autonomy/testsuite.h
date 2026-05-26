b96f764541e8812b3a9b1d5b8468d2652e7bcef3
Derker2002 <siklerzp@gmail.com>
2026-05-26 15:45:35 +0200

    fix

--- f4_autonomy/testsuite.h
+++ f4_autonomy/testsuite.h
@@ -164,74 +164,10 @@ static void mavlink_test_f4_detector(uint8_t system_id, uint8_t component_id, ma
 #endif
 }
 
-static void mavlink_test_zoom_to_region(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
-{
-#ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
-    mavlink_status_t *status = mavlink_get_channel_status(MAVLINK_COMM_0);
-        if ((status->flags & MAVLINK_STATUS_FLAG_OUT_MAVLINK1) && MAVLINK_MSG_ID_ZOOM_TO_REGION >= 256) {
-            return;
-        }
-#endif
-    mavlink_message_t msg;
-        uint8_t buffer[MAVLINK_MAX_PACKET_LEN];
-        uint16_t i;
-    mavlink_zoom_to_region_t packet_in = {
-        93372036854775807ULL,73.0,101.0,129.0,157.0
-    };
-    mavlink_zoom_to_region_t packet1, packet2;
-        memset(&packet1, 0, sizeof(packet1));
-        packet1.timestamp = packet_in.timestamp;
-        packet1.x = packet_in.x;
-        packet1.y = packet_in.y;
-        packet1.width = packet_in.width;
-        packet1.height = packet_in.height;
-        
-        
-#ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
-        if (status->flags & MAVLINK_STATUS_FLAG_OUT_MAVLINK1) {
-           // cope with extensions
-           memset(MAVLINK_MSG_ID_ZOOM_TO_REGION_MIN_LEN + (char *)&packet1, 0, sizeof(packet1)-MAVLINK_MSG_ID_ZOOM_TO_REGION_MIN_LEN);
-        }
-#endif
-        memset(&packet2, 0, sizeof(packet2));
-    mavlink_msg_zoom_to_region_encode(system_id, component_id, &msg, &packet1);
-    mavlink_msg_zoom_to_region_decode(&msg, &packet2);
-        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
-
-        memset(&packet2, 0, sizeof(packet2));
-    mavlink_msg_zoom_to_region_pack(system_id, component_id, &msg , packet1.x , packet1.y , packet1.width , packet1.height , packet1.timestamp );
-    mavlink_msg_zoom_to_region_decode(&msg, &packet2);
-        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
-
-        memset(&packet2, 0, sizeof(packet2));
-    mavlink_msg_zoom_to_region_pack_chan(system_id, component_id, MAVLINK_COMM_0, &msg , packet1.x , packet1.y , packet1.width , packet1.height , packet1.timestamp );
-    mavlink_msg_zoom_to_region_decode(&msg, &packet2);
-        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
-
-        memset(&packet2, 0, sizeof(packet2));
-        mavlink_msg_to_send_buffer(buffer, &msg);
-        for (i=0; i<mavlink_msg_get_send_buffer_length(&msg); i++) {
-            comm_send_ch(MAVLINK_COMM_0, buffer[i]);
-        }
-    mavlink_msg_zoom_to_region_decode(last_msg, &packet2);
-        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
-        
-        memset(&packet2, 0, sizeof(packet2));
-    mavlink_msg_zoom_to_region_send(MAVLINK_COMM_1 , packet1.x , packet1.y , packet1.width , packet1.height , packet1.timestamp );
-    mavlink_msg_zoom_to_region_decode(last_msg, &packet2);
-        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
-
-#ifdef MAVLINK_HAVE_GET_MESSAGE_INFO
-    MAVLINK_ASSERT(mavlink_get_message_info_by_name("ZOOM_TO_REGION") != NULL);
-    MAVLINK_ASSERT(mavlink_get_message_info_by_id(MAVLINK_MSG_ID_ZOOM_TO_REGION) != NULL);
-#endif
-}
-
 static void mavlink_test_f4_autonomy(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
 {
     mavlink_test_companion_version(system_id, component_id, last_msg);
     mavlink_test_f4_detector(system_id, component_id, last_msg);
-    mavlink_test_zoom_to_region(system_id, component_id, last_msg);
 }
 
 #ifdef __cplusplus
